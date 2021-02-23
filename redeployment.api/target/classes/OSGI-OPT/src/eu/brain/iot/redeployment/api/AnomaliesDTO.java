9f4b7594e41e239bad4dc1d1b356cc57e2a4007c
xu <sunshine930112@gmail.com>
2021-02-23 19:47:56 +0100

    rollback to original

--- redeployment.api/target/classes/OSGI-OPT/src/eu/brain/iot/redeployment/api/AnomaliesDTO.java
+++
@@ -1,8 +0,0 @@
-package eu.brain.iot.redeployment.api;
-
-import eu.brain.iot.eventing.api.BrainIoTEvent;
-
-public class AnomaliesDTO extends BrainIoTEvent{
-   public String Scenario;
-   public String RobotID;
-}
