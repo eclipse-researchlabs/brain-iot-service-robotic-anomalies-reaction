4381513bc88aeff6f7a606f57efd6f2d0b1f7158
TaoXu00 <xu.tao@linksfoundation.com>
2021-04-29 13:17:05 +0200

    change id to robotID in the startROSDTO

--- startButton.api/src/main/java/eu/brain/iot/service/robotic/startButton/api/StartROSDTO.java
+++ startButton.api/src/main/java/eu/brain/iot/service/robotic/startButton/api/StartROSDTO.java
@@ -3,8 +3,8 @@ package eu.brain.iot.service.robotic.startButton.api;
 import eu.brain.iot.eventing.api.BrainIoTEvent;
 
 public class StartROSDTO extends BrainIoTEvent{
- public int id;
- public StartROSDTO(int id) {
-	 this.id=id;
+ public int robotID;
+ public StartROSDTO(int robotID) {
+	 this.robotID=robotID;
  }
 }
